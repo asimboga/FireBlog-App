d30cac455d59b9e918c7cf43c0b550a03b02154e
asimboga <asimboga@gmail.com>
2022-07-30 20:49:21 +0300

    router

---
+++ src/app-router/AppRouter.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import About from '../pages/About'
+import Dashboard from '../pages/Dashboard'
+import Details from "../pages/Details" 
+import Login from "../pages/Login"
+import NewBlog from '../pages/NewBlog'
+import Profile from '../pages/Profile'
+import Register from "../pages/Register"
+import UpdateBlog from '../pages/UpdateBlog'
+import Navbar from '../components/Navbar'
+
+
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+        {/* <Navbar/> */}
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/about/:id' element={<About/>}/>
+          <Route path='/details/:id' element={<Details/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/newblog/:id' element={<NewBlog/>}/>
+          <Route path='/profile/:id' element={<Profile/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/updateblog/:id' element={<UpdateBlog/>}/>
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter;
---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createContext } from 'react';
+
+
+// export const AuthContext = createContext();
+const AuthContext = () => {
+  return (
+    <div>AuthContext</div>
+  )
+}
+
+export default AuthContext
